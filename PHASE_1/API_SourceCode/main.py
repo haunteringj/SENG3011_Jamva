a869798c0d25c8cb0f6dd7effcd1bc99ae6dfe59
Marko <nfslolmiku@gmail.com>
2022-03-14 20:05:06 +1100

    added a logger for tracking perfromance

--- PHASE_1/API_SourceCode/main.py
+++ PHASE_1/API_SourceCode/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import requests
+import time
+import datetime
 try: 
   from articleEndPoints import *
 except:
@@ -38,3 +41,59 @@ def fetchByDis(disease):
 @app.get("/articles/search/date")
 def fetchByDate(startDate, endDate = ""):
   return fetchByDateArticle(db, startDate, endDate)
+
+# logger (keeps track of API performance) Runs for each request of the api
+@app.middleware("http")
+async def Logger(request: Request, call_next):
+  # Track which endpoint this log is for
+  parts = str(request.url)
+  parts = parts.split('/')[3:]
+  endpoint = "/" + '/'.join(parts)
+
+  # Tracks reponse time of request
+  startTime = time.time()
+  response = await call_next(request)
+  endTime = time.time()
+
+  # Track time of request
+  requestTime = str(datetime.now())
+  processTime = str(round((endTime - startTime), 4)) + "s"
+
+  # Track Status of request
+  status = str(response.status_code)
+  
+  # Track method used
+  method = request.method
+  
+  # Track user ip
+  ip = str(request.client.host)
+  # ip = ipToLocation(request.client.host)
+
+  # Track request packet body size (bytes)
+  size = response.headers['content-length']
+  
+  logFile = open('./log.txt', 'a')
+  logEntry = endpoint + " [" + method + ", " + status + ", " + ip + ", " + requestTime + ", " + processTime + ", " + size + "B]\n" 
+  logFile.write(logEntry)
+  return response
+
+
+# Used to find the location of ip address 
+# *Not Used for now as it slows down response time considerably
+def ipToLocation(ip):
+  # use a website to find the location of ip
+  requestUrl = 'https://geolocation-db.com/jsonp/' + ip
+  try:
+    response = requests.get(requestUrl)
+  except:
+    return "ip to location website is currently unavailable"
+  
+  # decode content
+  result = response.content.decode()
+
+  # remove uncessary infomation
+  result = result.split("(")[1].strip(")")
+
+  # Convert date into a json dictionary
+  result = json.loads(result)
+  return result['country_name'] + ", " + result['city']
