88162427873e58a3987ad77c7595576eec12f2b6
Vishnu <vpgit123@gmail.com>
2022-03-18 15:43:30 +1100

    updated working scraper]

--- PHASE_1/API_SourceCode/scraper.py
+++ PHASE_1/API_SourceCode/scraper.py
@@ -4,9 +4,7 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
-import pycountry
 import json
-
 from firebase_admin import credentials
 from firebase_admin import firestore
 import firebase_admin
@@ -24,10 +22,70 @@ postid = 0
 url2 = "https://promedmail.org/promed-post/?place="
 
 
-driver = webdriver.Safari()
+driver = webdriver.Chrome()
+
+driver.get(url1)
+
+# get the latest date from the db to check if there are any new posts
+# if no posts, termintate scraper
+# If there are new posts then:
+# get the the ids of all those posts
+# get all the info about those posts
+# create a list
+# push the list to the database
+
+wait = WebDriverWait(driver, 10)
+latest_id_list = []
+countries = ['Afghanistan', 'USA', 'Europe', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg',
+             'Macao', 'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe']
 
 
-def getArticleData(element):
+# get all the ids of the latest posts
+try:
+    print("fetching latest list")
+    fail = True
+    while fail:
+        try:
+            wait.until(EC.presence_of_element_located((By.XPATH, "//a[@class='lcl']")))
+            fail = False
+        except:
+            print("failed fetching latest list")
+            driver.get(url1)
+   
+    print("latest list loaded")
+    list_div = driver.find_element(By.XPATH, "//div[@class='latest_list']/ul")
+    items = list_div.find_elements(By.TAG_NAME, value="li")
+
+    print("--------------------------------------")
+    num = 1
+    # print(items)]
+    print(len(items))
+    for item in items:
+        link = item.find_element(By.TAG_NAME, value="a")
+        # id = re.search('\d{7}', link.get_attribute("id"))÷
+        id = link.get_attribute("id")[2:]
+        title = link.text
+        date = driver.execute_script(
+            'return arguments[0].firstChild.textContent;', item).strip()
+        ct = ''
+        for c in countries:
+            if c in title:
+                ct = c
+
+        latest_id_list.append(
+            {'id': id, 'title': title, 'date': date, 'country': ct})
+
+except Exception as e:
+    print(e)
+    print("fail")
+
+driver.close()
+
+# for each id in the list get the article data
+count = 0
+for element in latest_id_list:
+    if count == 20:
+        break
     driver1 = webdriver.Safari()
 
     wait = WebDriverWait(driver1, 10)
@@ -72,9 +130,7 @@ def getArticleData(element):
         # if mt:
         #     element['main'] = mt
         # else:
-        #     element['main'] = main_text.text
-            
-        
+        #     element['main'] = main_text.text 
     except Exception as e:
         element['main'] = ''
         print(e)
@@ -88,106 +144,17 @@ def getArticleData(element):
         element['original_source'] = ''
         print(e)
 
-    driver1.close()
-
-
-driver.get(url1)
-
-# get the latest date from the db to check if there are any new posts
-# if no posts, termintate scraper
-# If there are new posts then:
-# get the the ids of all those posts
-# get all the info about those posts
-# create a list
-# push the list to the database
-
-wait = WebDriverWait(driver, 10)
-latest_id_list = []
-countries = ['Afghanistan', 'USA', 'Europe', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg',
-             'Macao', 'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe']
-
-
-# get all the ids of the latest posts
-try:
-    print("fetching latest list")
-    fail = True
-    while fail:
-        try:
-            wait.until(EC.presence_of_element_located((By.XPATH, "//a[@class='lcl']")))
-            fail = False
-        except:
-            print("failed fetching latest list")
-            driver.get(url1)
-   
-    print("latest list loaded")
-    # if this is the first time the scraper is running
-    # time.sleep(5)
-    list_div = driver.find_element(By.XPATH, "//div[@class='latest_list']/ul")
-    items = list_div.find_elements(By.TAG_NAME, value="li")
-
-    # print(list_div.get_attribute("innerHTML"))
-    print("--------------------------------------")
-    num = 1
-    # print(items)]
-    print(len(items))
-    for item in items:
-        link = item.find_element(By.TAG_NAME, value="a")
-        # id = re.search('\d{7}', link.get_attribute("id"))÷
-        id = link.get_attribute("id")[2:]
-        title = link.text
-        date = driver.execute_script(
-            'return arguments[0].firstChild.textContent;', item).strip()
-        ct = ''
-        for c in countries:
-            if c in title:
-                ct = c
-
-        latest_id_list.append(
-            {'id': id, 'title': title, 'date': date, 'country': ct})
-
-except Exception as e:
-    print(e)
-    print("fail")
-    # print(latest_id_list)
-    # driver.close()
-    # exit(1)
-
-
-driver.close()
-
-# for each id in the list get the article data
-count = 0
-for element in latest_id_list:
-    if count == 10:
-        break
-    getArticleData(element)
-    count += 1
-
-
-
-# \collection\docid
-count = 0
-for e in latest_id_list:
-    if count == 10:
-        break
+    print(element)
     a = {
-        "date_of_publication": e['published_date'],
-        "headline":e['title'],
-        "id":id,
-        "main_text":e['main'],
+        "date_of_publication": element['published_date'],
+        "headline":element['title'],
+        "id":element['id'],
+        "main_text": element['main'],
         "reports":[],
-        "url": e['original_source']
+        "url": element['original_source']
     }
-    db.collection("articles").document(e['id']).set(a)
-
-    report = {
-   "article": '',
-   "cases": 0, 
-   "diseases": [],
-   "event_date": e['published_date'],
-   "locations":[],
-}
+    db.collection("articles").document(element['id']).set(a)
+    
+    driver1.close()
     count += 1
 
-
-print(json.dumps(latest_id_list))
