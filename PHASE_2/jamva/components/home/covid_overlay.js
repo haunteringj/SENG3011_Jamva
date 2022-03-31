67fa82ec65f42f4ed0c5939939dc190b177845c5
Marko <nfslolmiku@gmail.com>
2022-03-31 15:28:01 +1100

    fixed overlay and tags bug

--- PHASE_2/jamva/components/home/covid_overlay.js
+++ PHASE_2/jamva/components/home/covid_overlay.js
@@ -25,15 +25,15 @@ const ContinentCovidData = (props) => {
 
     // return component
     return (
-        <div>
-            <h><b>{props.continent}'s Covid-19</b></h>
-            <body>
+        <div className={styles.sidebar}>
+            <b>{props.continent}'s Covid-19{"\n"}</b>
+            <p>
                 <b>Cases: </b>{data["cases"]}{"\n"}
                 <b>Today Cases: </b>{data["todayCases"]}{"\n"}
                 <b>Deaths: </b>{data["deaths"]}{"\n"}
                 <b>Today Deaths: </b>{data["todayDeaths"]}{"\n"}
                 <b>Tests: </b>{data["tests"]}
-            </body>
+            </p>
         </div>
     )
 }
--- PHASE_2/jamva/components/home/earth.js
+++ PHASE_2/jamva/components/home/earth.js
@@ -1,4 +1,3 @@
-import dynamic from 'next/dynamic';
 import CountryData from './covid_overlay';
 import TopDiseases from './earth_overlay';
 import { useState, useEffect, useRef, useCallback } from 'react';  
@@ -7,9 +6,7 @@ import styles from '../../styles/Home.module.scss'
 // Earth 
 function Earth () {
     const [latestContinent, setLatestContinent]= useState("World");
-    const [latestCountry, setLatestCountry]= useState("World");
     // import basic globe
-    // const Globe = dynamic(import('react-globe.gl'), { ssr: false });
     let Globe = () => null;
     if (typeof window != 'undefined') {
         Globe = require('react-globe.gl').default;
@@ -20,7 +17,7 @@ function Earth () {
     const [countries, setCountries] = useState({ features: []});
     // loads data onto polygon layer (Does not reload the whole globe)
     useEffect(() => {
-        console.log("reloading the earth")
+        console.log("loading the earth")
         fetch('/countries.geojson').then(res => res.json())
         .then(countries=> {
             setCountries(countries);
@@ -38,7 +35,6 @@ function Earth () {
         // Update overlays based on the clicked continent (ignore Antarctica, small islands)
         if (polygon.properties.CONTINENT != "Antarctica" && polygon.properties.CONTINENT != "Seven seas (open ocean)" ){
             setLatestContinent(polygon.properties.CONTINENT)
-            setLatestCountry(polygon.properties.NAME)
         }
     }, []);
 
@@ -59,12 +55,12 @@ function Earth () {
                 <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
                 Population: <i>${Math.round(+d.POP_EST / 1e4) / 1e2}M</i>
             `}
-            />;
-            <div className={styles.overlay}>
+            />
+            <div>
                 <TopDiseases continent={latestContinent}/>
-            </div>
-            <div className={styles.sidebar}>
-                <CountryData continent={latestContinent}/>
+                <div>
+                    <CountryData continent={latestContinent}/>
+                </div>
             </div>
         </div>
     )
