170bb481a19b43683b537af4b939c645abbd5d7c
Vishnu <vpgit123@gmail.com>
2022-04-04 11:05:53 +1000

    added report overlay functinality and styling for overlay

--- PHASE_2/jamva/components/home/report_overlay.js
+++ PHASE_2/jamva/components/home/report_overlay.js
@@ -1,40 +1,72 @@
 import react from "react";
 import Link from "next/link"
 import styles from '../../styles/Home.module.scss'
+import { Box } from '@chakra-ui/react'
+import ReportBox from "./report_box";
 
-export default function ReportOverlay({ report }) {
 
-    const [visible, setVisible] = react.useState(false)
+export default function ReportOverlay({ reports }) {
 
-    react.useEffect(() => {
-        if (report !== {}) setVisible(true);
-        else setVisible(false);
-    }, [report]);
+  const [visible, setVisible] = react.useState(true)
 
-    console.log(report);
+  // console.log(reports);
+  
+  // react.useEffect(() => {
+  //   if (reports !== []) setVisible(true);
+  //   else setVisible(false);
 
-    return ( visible ? 
-        <div>
-            <h> <b>Reports</b></h>
-            <body>
-            <div className={styles.diseaseList}>
-                <ol>
-                    <li>
-                    
-                    {report["headline"]}
-                    hello
-                    </li>
-                    {/* <li>
-                    <Link href="/user">{D2}</Link>
-                    </li>
-                    <li>
-                    <Link href="/user">{D3}</Link>
-                    </li> */}
-                </ol>
-                </div>
-            </body>
-        </div> : null
-    )
+  //   console.log(reports)
+  // }, [reports]);
+
+
+  return (visible ?
+
+    <Box display="flex" flexDir="column" overflowY="scroll" w='sm' h='sm' borderWidth='1px' borderRadius='lg' opacity={0.5} overflow='hidden' backgroundColor={'whitesmoke'}>
+      <Box p='3'>
+        <Box
+          mt='1'
+          fontWeight='Bold'
+          fontSize={25}
+          as='h1'
+          lineHeight='tight'
+          isTruncated
+          textAlign="center"
+        >
+          Reports
+        </Box>
+        {/* <Box display='flex' alignItems='baseline'>
+             
+              <Box
+                color='gray.500'
+                fontWeight='semibold'
+                letterSpacing='wide'
+                fontSize='xs'
+                textTransform='uppercase'
+                ml='2'
+              >
+                Some other text
+              </Box>
+            </Box> */}
+          <Box>
+        {reports.map((r, i) => (
+          <ReportBox key={i} title={r['headline']} id={r['id']} date={r['date']} />
+        ))}
+        </Box>
+
+
+
+
+
+        {/* <Box>
+              DATE
+              <Box as='span' color='gray.600' fontSize='sm'>
+                / wk
+              </Box>
+            </Box> */}
+
+      </Box>
+    </Box> : null
+  )
 
 
 
--- PHASE_2/jamva/pages/_app.js
+++ PHASE_2/jamva/pages/_app.js
@@ -1,14 +1,17 @@
 import Layout from '../components/global/layout'
 import { UserWrapper } from '../context/userState'
 import '../styles/globals.scss'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <ChakraProvider>
     <UserWrapper>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </UserWrapper>
+    </ChakraProvider>
   )
 }
 
