8e0cb6b32fc6ccc130fcb80959c0f5efc18b0053
Jesse Merhi <j.merhi@student.unsw.edu.au>
2022-04-04 16:15:45 +1000

    More changes

--- PHASE_2/jamva/pages/index.js
+++ PHASE_2/jamva/pages/index.js
@@ -1,10 +1,11 @@
-import styles from '../styles/Home.module.scss'
-import Earth from '../components/home/earth';
-
+import styles from "../styles/Home.module.scss";
+import Earth from "../components/home/earth";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Earth />
-  )
+    <ChakraProvider>
+      <Earth />
+    </ChakraProvider>
+  );
 }
-
